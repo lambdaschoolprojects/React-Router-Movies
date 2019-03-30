77aa72aa09acfcf71e2b0fa3f7d9c1fb4c1669d3
Rane Wallin <RAN2143066@maricopa.edu>
2019-03-29 18:26:48 -0700

    stretch: created MovieCard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -33,26 +35,10 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
+  const { id } = movie;
   return (
       <Link to={`/movies/${id}`}>
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))
-      }
-    </div>
+        <MovieCard {...movie} />
       </Link>
   );
 }
